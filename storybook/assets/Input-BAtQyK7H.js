73efe9fd992b8fa0ca966eb01cc601fa85ae8f65
inaemin <inaemin@gmail.com>
2026-05-01 17:28:41 +0900

    Updates

---
+++ storybook/assets/Input-BAtQyK7H.js
@@ -0,0 +1,29 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{a as t,n,o as r,r as i}from"./emotion-react-jsx-runtime.browser.esm-DFFSHqNi.js";function a({placeholder:e=`1234`,value:t,type:n,onChange:r,variant:a=`default`,...c}){return i(`input`,{...c,placeholder:e,value:t,type:n,onChange:r,css:[o,s[a]]})}var o,s,c=e((()=>{r(),n(),o=t`
+  width: 100%;
+  height: 32px;
+  border-radius: 2px;
+  border: 1px solid;
+  padding: 8px;
+
+  font-size: 11px;
+  line-height: 135%;
+
+  ::placeholder {
+    font-size: 11px;
+    line-height: 135%;
+    color: var(--color-text-placeholder);
+  }
+
+  :focus {
+    outline: none;
+    border: 1px solid var(--color-border-focus);
+  }
+`,s={default:t`
+    border-color: var(--color-border-default);
+  `,error:t`
+    border-color: var(--color-border-error);
+
+    :focus {
+      border-color: var(--color-border-error);
+    }
+  `},a.__docgenInfo={description:``,methods:[],displayName:`Input`,props:{variant:{required:!1,tsType:{name:`union`,raw:`'default' | 'error'`,elements:[{name:`literal`,value:`'default'`},{name:`literal`,value:`'error'`}]},description:``,defaultValue:{value:`'default'`,computed:!1}},placeholder:{defaultValue:{value:`'1234'`,computed:!1},required:!1}}}}));export{c as n,a as t};
---
+++ storybook/assets/Input.stories-g6VwkvfT.js
@@ -0,0 +1,19 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{n as t,r as n}from"./emotion-react-jsx-runtime.browser.esm-DFFSHqNi.js";import{n as r,t as i}from"./Input-BAtQyK7H.js";var a,o,s,c,l,u;e((()=>{r(),t(),{fn:a}=__STORYBOOK_MODULE_TEST__,o={title:`ui/Input`,component:i,parameters:{layout:`centered`},tags:[`autodocs`],args:{placeholder:`1234`,type:`text`,onChange:a()}},s={args:{value:`1234`,variant:`default`},render:e=>n(i,{...e})},c={args:{value:``,variant:`error`},render:e=>n(i,{...e})},l={args:{value:``,variant:`default`},render:e=>n(i,{...e,autoFocus:!0})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: '1234',
+    variant: 'default'
+  },
+  render: args => <Input {...args} />
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: '',
+    variant: 'error'
+  },
+  render: args => <Input {...args} />
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: '',
+    variant: 'default'
+  },
+  render: args => <Input {...args} autoFocus />
+}`,...l.parameters?.docs?.source}}},u=[`Default`,`Error`,`Focus`]}))();export{s as Default,c as Error,l as Focus,u as __namedExportsOrder,o as default};
